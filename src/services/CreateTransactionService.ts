b01998b8fd9b203064471924b89e7576cfcaa99e
Rafael Yanagui <rafael.yanagui@gmail.com>
2020-04-29 22:48:57 -0300

    Finaliza desafio

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: string;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,33 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const valueConvertedNumber = Number(value);
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (Number.isNaN(valueConvertedNumber)) {
+      throw Error(
+        'Value field is invalid number, please inform a valid number (Example: 100.50).',
+      );
+    }
+
+    if (type !== 'income' && type !== 'outcome') {
+      throw Error(
+        'Type field is invalid, please inform the type "income" or "outcome".',
+      );
+    }
+
+    if (type === 'outcome' && valueConvertedNumber > total) {
+      throw Error('Balance insuficient. You don`t have enough balance.');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value: valueConvertedNumber,
+      type,
+    });
+
+    return transaction;
   }
 }
 
